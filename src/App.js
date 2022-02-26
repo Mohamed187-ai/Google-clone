42e619605744920bf3bacb34db920c347f66d131
Mohamed Hassan <mohassan787898@gmail.com>
2022-02-26 21:54:13 +0200

    add tailwind

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi Mo</h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">Hi Mo</h1>
     </div>
   );
 }
