b9947daf5dbb586ee094d43ff11c68aaf3ec1d85
Mohamed Hassan <mohassan787898@gmail.com>
2022-02-27 17:29:02 +0200

    add search structure

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
-
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Results } from './Results'
 
 export const Search = () => {
   return (
-    <div>Search</div>
+    <div className='p-4'>
+      <Routes>
+        <Route path='/' element={<Navigate to='/search' />} />
+        {/* <Route path={['/search', 'images', 'news', 'videos']} element={<Results />} /> */}
+        {['/search', 'images', 'news', 'videos'].map((path, index) => 
+          <Route path={path} element={<Results />} />
+        )}
+      </Routes>
+    </div>
   )
 }
